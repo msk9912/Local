9db9d7d220a3cef39dd90464b966428abffa9e7c
Ms Kim <myungseokim9912@gmail.com>
2022-05-06 23:31:37 +0900

    firebase third commit

--- app/src/main/java/com/duksung/local/RegisterActivity.java
+++ app/src/main/java/com/duksung/local/RegisterActivity.java
@@ -3,7 +3,10 @@ package com.duksung.local;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,12 +20,15 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 //회원가입
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final String TAG = "RegisterActivity";
     private FirebaseAuth mFirebaseAuth; //파이어베이스 인증
     private DatabaseReference mDatabaseRef; //실시간 데이터베이스
-    private EditText mEtEmail, mEtPwd;  //회원가입 입력필드
+    private EditText mEtEmail, mEtPwd, mEtPwd2, mEtNickname;  //회원가입 입력필드
     private Button mBtnRegister;    //회원가입 버튼
 
     @Override
@@ -35,38 +41,78 @@ public class RegisterActivity extends AppCompatActivity {
 
         mEtEmail = findViewById(R.id.et_email);
         mEtPwd = findViewById(R.id.et_pwd);
+        mEtPwd2 = findViewById(R.id.et_pwd2);
+        mEtNickname = findViewById(R.id.et_nickname);
         mBtnRegister = findViewById(R.id.btn_register);
 
-        mBtnRegister.setOnClickListener(new View.OnClickListener()
-        {
+        //파이어베이스에 데이터를 저장
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                //회원가입 처리
+            public void onClick(View view) {
+                //회원가입 정보 가져오기
                 String strEmail = mEtEmail.getText().toString();
                 String strPwd = mEtPwd.getText().toString();
+                String strPwd2 = mEtPwd2.getText().toString();
+//                String strNickname = mEtNickname.getText().toString();
 
-                //Firebase Auth 진행
-                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
-                            UserAccount account = new UserAccount();
-                            account.setIdToken(firebaseUser.getUid());
-                            account.setEmailId(firebaseUser.getEmail());
-                            account.setPassword(strPwd);
+                if (strPwd.equals(strPwd2)) {
+                    Log.d(TAG, "등록 버튼 " + strEmail + " , " + strPwd);
+                    final ProgressDialog mDialog = new ProgressDialog(RegisterActivity.this);
+                    mDialog.setMessage("가입중입니다...");
+                    mDialog.show();
 
-                            // setValue : database에 insert (삽입) 행위
-                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                    //파이어베이스에 신규계정 등록하기
+                    mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
 
-                            Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다", Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(RegisterActivity.this, "회원가입에 실패하셨습니다", Toast.LENGTH_SHORT).show();
+                            //가입 성공시
+                            if (task.isSuccessful()) {
+                                mDialog.dismiss();
+
+                                FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                                String email = user.getEmail();
+                                String uid = user.getUid();
+                                String pwd = mEtPwd.getText().toString();
+                                String nickname = mEtNickname.getText().toString().trim();
+
+                                //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+                                HashMap<Object, String> hashMap = new HashMap<>();
+
+                                hashMap.put("uid", uid);
+                                hashMap.put("email", email);
+                                hashMap.put("nickname", nickname);
+
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference reference = database.getReference("Users");
+                                reference.child(uid).setValue(hashMap);
+
+
+                                //가입이 이루어져을시 가입 화면을 빠져나감.
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                                Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                mDialog.dismiss();
+                                Toast.makeText(RegisterActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                                return;  //해당 메소드 진행을 멈추고 빠져나감.
+
+                            }
+
                         }
-                    }
-                });
+                    });
+
+                    //비밀번호 오류시
+                } else {
+
+                    Toast.makeText(RegisterActivity.this, "비밀번호가 틀렸습니다. 다시 입력해 주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
             }
         });
+
     }
+
 }
